8279cc7e62bc350f7785d345fc0ddcfd53801083
TaronovRmn <taronovroman@mail.ru>
2019-04-09 13:55:24 +0700

    Add project files.

---
+++ EngineTester/EngineTester.cpp
@@ -0,0 +1,26 @@
+﻿// EngineTester.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ICEngine {
+	double i;
+	vector<double> m;
+	vector<double> v;
+	double t_max;
+	double h_m;
+	double h_v;
+	double c;
+public:
+	start
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
